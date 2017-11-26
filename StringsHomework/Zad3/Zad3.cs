8cf811aefdb4e77e4760a4cb1303f6546945a145
Unknown <marti_stamatov@abv.bg>
2017-11-26 12:03:20 +0200

    Zad3 done

---
+++ StringsHomework/Zad3/Zad3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad3
+{
+    class Zad3
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isCorrect = true;
+            int open = 0;
+            int closed = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    open++;
+                }
+                else if (input[i] == ')')
+                {
+                    closed++;
+                }
+                if (open<closed)
+                {
+                    isCorrect = false;
+                }
+
+            }
+            if (open != closed)
+            {
+                isCorrect = false;
+            }
+            if (isCorrect)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+        }
+    }
+}
